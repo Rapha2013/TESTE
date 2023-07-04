55859dcec060286ec109674bee108518f92bfe54
Raphael <rapha_2gtba@hotmail.com>
2023-07-04 17:01:58 -0300

    first commit

---
+++ app/Models/Product.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $table = 'product';
+
+     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'id',
+        'name',
+        'value',
+        'user_id',
+        'category_id',
+        'created_at',
+        'updated_at'
+    ];
+
+}
+
